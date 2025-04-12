c66db87a51bf8795d88ceb6689ccc526cee90aba
nivasmukte <nivasmukte81@gmail.com>
2025-04-12 22:17:59 +0530

    added testng xml for parallel execution

--- src/test/java/feature/login.feature
+++ src/test/java/feature/login.feature
@@ -2,12 +2,12 @@
 Feature: Login functionality check
   Background: for loading a login page
     Given User is on login page
-    @validlogin
+  @validlogin
   Scenario: Login check with valid credentials
     When User enters valid credentials
     Then User logged in successfully
 
-      @invalidlogin
+  @invalidlogin
   Scenario Outline: Login check with invalid creadentials
     When user enters invalid username "<userName>" and password "<Password>"
     Then Uesr not able to login
--- src/test/java/runner/testRunner.java
+++ src/test/java/runner/testRunner.java
@@ -1,15 +1,24 @@
 package runner;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/feature",
         glue="stepDefinition",
-        tags="@validlogin",
+        tags="@smoketest",
         plugin = {"pretty","html:target/cucumber-reports.html"}
 )
-public class testRunner {
+public class testRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel=true)
+    public Object[][] scenarios(){
+        return super.scenarios();
+    }
+
 }
